6088e9af4b5769fd28ef048c77c930497eb51b10
tesbro1194 <tes1194@naver.com>
2024-10-08 17:30:01 +0900

    쿠키 생성하기, 건내주기

---
+++ src/main/java/com/sparta/springauth/auth/AuthController.java
@@ -0,0 +1,47 @@
+package com.sparta.springauth.auth;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    @GetMapping("/create-cookie")
+    public String createCookie(HttpServletResponse res) {
+        addCookie("Robbie Auth", res);
+        return "createCookie";
+    }
+
+    @GetMapping("/get-cookie")
+    // @CookieValue(AUTHORIZATION_HEADER) : () 안에 건내줄 쿠키의 Name 을 넣으면 변수 value 에 그 쿠키의 Value 가 들어감
+    public String getCookie(@CookieValue(AUTHORIZATION_HEADER) String value) {
+        System.out.println("value = " + value);
+
+        return "getCookie : " + value;
+    }
+
+    public static void addCookie(String cookieValue, HttpServletResponse res) {
+        try {
+            cookieValue = URLEncoder.encode(cookieValue, "utf-8").replaceAll("\\+", "%20"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
+
+            Cookie cookie = new Cookie(AUTHORIZATION_HEADER, cookieValue); // 쿠키의 Name-Value
+            cookie.setPath("/");
+            cookie.setMaxAge(30 * 60);
+
+            // Response 객체에 Cookie 추가
+            res.addCookie(cookie);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
