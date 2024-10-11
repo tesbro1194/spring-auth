6dbd23e8d85ad28d8c146bf4dcc16332daea1839
tesbro1194 <tes1194@naver.com>
2024-10-11 15:58:00 +0900

    접근 불가 페이지

--- src/main/java/com/sparta/springauth/controller/ProductController.java
+++ src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.sparta.springauth.controller;
 
 import com.sparta.springauth.entity.User;
+import com.sparta.springauth.entity.UserRoleEnum;
 import com.sparta.springauth.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,12 +23,14 @@ public class ProductController {
         System.out.println("user.getEmail() = " + user.getEmail());
         return "redirect:/";
     }
-/*    @GetMapping("/products")
-    public String getProducts(HttpServletRequest req) {
-        System.out.println("ProductController.getProducts : 인증 완료");
-        User user = (User) req.getAttribute("setUser");
-        System.out.println("user.getUsername() = " + user.getUsername());
+    @Secured(UserRoleEnum.Authority.ADMIN) // 관리자용
+    @GetMapping("/products/secured")
+    public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+        for (GrantedAuthority authority : userDetails.getAuthorities()) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
 
         return "redirect:/";
-    }*/
+    }
 }
